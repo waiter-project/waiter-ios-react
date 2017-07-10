6124434dd80f6e8dafa17b06217f94ebaaf719a8
François de Rochebrune <fdr@imac-de-francois.home>
2017-07-10 15:33:12 +0000

    Map ok

--- src/ui/pages/map/map_screen.js
+++ src/ui/pages/map/map_screen.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { ScrollView, View } from 'react-native';
+import { ScrollView, View, Dimensions } from 'react-native';
 import {
   Text
 } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { EventsActions } from '../../../actions';
-//import MapView from 'react-native-maps';
+import MapView, { MAP_TYPES, PROVIDER_DEFAULT } from 'react-native-maps';
 
 // -------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------
@@ -13,16 +13,35 @@ import { EventsActions } from '../../../actions';
 
 import mapStyle from './map_style';
 
+const { width, height } = Dimensions.get('window');
+
+console.log(width, height)
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
 // -------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------
+function randomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
 class MapScreen extends Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
-      events: []
+      events: [],
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      }
     };
   }
 
@@ -33,25 +52,55 @@ class MapScreen extends Component {
     dispatch(EventsActions.getAllEvents());
   }
 
-
   componentWillReceiveProps(nextProps) {
-      this.setState({
-          events: nextProps.event.toJS()
-      });
+    this.setState({
+      events: nextProps.event.toJS().events
+    });
   }
 
-  componentDidMount () {
+  componentDidMount() {
+  }
+
+  _generateMarker(events) {
+    console.log("et coucou : ", events);
+    return events.map(marker => (
+      <MapView.Marker
+        key={marker._id}
+        coordinate={{
+          latitude: marker.location[1],
+          longitude: marker.location[0]
+        }}
+        pinColor={randomColor()}
+        title={marker.name}
+        description={marker.description}
+      />
+    ))
   }
 
   // ----------------------------------------------------------------------
 
+
+
   render() {
-    console.log(this.state);
+    const { region } = this.state.region;
+    const events = this.state.events;
     return (
-      <ScrollView style={{ backgroundColor: 'white' }}>
-        <View style={mapStyle.containers}>
-        </View>
-      </ScrollView>
+      <View style={mapStyle.container}>
+        <MapView
+          style={mapStyle.map}
+          mapType={MAP_TYPES.STANDARD}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          {
+            this._generateMarker(events)
+          }
+        </MapView>
+      </View>
     );
   } // <= render
 
--- src/ui/pages/map/map_style.js
+++ src/ui/pages/map/map_style.js
@@ -8,7 +8,21 @@ export default StyleSheet.create({
   hero: {
     padding: 10
   },
-  containers: {
-    padding: 5
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: 'red'
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   }
 });
